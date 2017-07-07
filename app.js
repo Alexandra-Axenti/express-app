1c0584f79aa019269cad619b98e6ac986a389364
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-07-07 18:00:22 +0800

    add views and templates using ejs

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine','ejs');
+
 app.get('/', function(req,res){
-  res.send('this is the homepage');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req,res){
-  res.send('this is the contact page');
+  res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:id', function(req,res){
-  res.send("You requested to see a profile with the id of " + req.params.id);
+  var data = {age: 29, job: 'ninja'};
+  res.render('profile', {person: req.params.id, data: data});
 });
 
 app.listen(3000);
